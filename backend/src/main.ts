48cc832227189aed3c85557c9a8e4ea2a1f54167
{krystellar} <krystel.zerep@gmail.com>
2025-06-12 23:17:56 +0800

    Revert "add/api-swagger-setup"
    This reverts commit a40a70af62336c01c7db59e2adffeb75d0cf3fbd.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Raketista')
-    .setDescription('API Docu for backend')
-    .setVersion('1.0')
-    // tags for sections
-    .addTag('raket', 'Endpoints related to RaketNow features')
-    .addTag('auth', 'Authentication and login APIs')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-// run the server then go to http://localhost:3000/api
