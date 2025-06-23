b6eb697b1dcde49fc20d8db5997527cc95a624b1
mj <jhayegnzls@gmail.com>
2025-06-23 23:09:56 +0800

    feature/tailwind-update

--- raketnow-frontend/src/App.jsx
+++ raketnow-frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    // <div>Hello World</div>
     <div className="text-3xl font-bold text-blue-500">Hello Tailwind!</div>
   )
 }
