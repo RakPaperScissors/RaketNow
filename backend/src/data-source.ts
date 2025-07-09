72c6f8ae53bb07fdff646d2ee36348d603ebcc24
{krystellar} <krystel.zerep@gmail.com>
2025-07-09 19:20:40 +0800

    feat: add skill seed

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,18 +1,18 @@
-// import {DataSource} from 'typeorm';
-// import {config} from 'dotenv';
-// import {Skills} from './entities/raketistaProfile/skills.entity';
+import {DataSource} from 'typeorm';
+import {config} from 'dotenv';
+import {Skills} from './entities/skills/entities/skill.entity';
 
-// config();
+config();
 
-// export const AppDataSource = new DataSource({
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT || '5432'),
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-//   entities: [Skills],
-//   synchronize: false,
-// });
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [Skills],
+  synchronize: false,
+});
 
 // run npx ts-node src/seed/seed-skills.ts to initialize
