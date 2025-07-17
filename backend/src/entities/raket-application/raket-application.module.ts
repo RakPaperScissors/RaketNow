4842b77adf0156e6f50d76c9dc1f20d854f947d2
{krystellar} <krystel.zerep@gmail.com>
2025-07-17 16:36:52 +0800

    chore: fixed variable names in service.ts

--- backend/src/entities/raket-application/raket-application.module.ts
+++ backend/src/entities/raket-application/raket-application.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RaketApplication } from './entities/raket-application.entity';
 import { RaketApplicationService } from './raket-application.service';
 import { RaketApplicationController } from './raket-application.controller';
 
 @Module({
-  controllers: [RaketApplicationController],
+  imports: [TypeOrmModule.forFeature([RaketApplication])],
   providers: [RaketApplicationService],
+  controllers: [RaketApplicationController],
+  exports: [RaketApplicationService],
 })
 export class RaketApplicationModule {}
