77df17c7bf9c094bd77c6cbf984bde44369bf7bb
{krystellar} <krystel.zerep@gmail.com>
2025-07-17 16:56:12 +0800

    chore: pause api and hook

--- frontend/src/hooks/useRaket.js
+++ frontend/src/hooks/useRaket.js
@@ -5,9 +5,9 @@ export function useRaket(id) {
     const [raket, setRaket] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
-    const [applyLoading, setApplyLoading] = useState(false);
-    const [applyError, setApplyError] = useState("");
-    const [applySuccess, setApplySuccess] = useState(false);
+    // const [applyLoading, setApplyLoading] = useState(false);
+    // const [applyError, setApplyError] = useState("");
+    // const [applySuccess, setApplySuccess] = useState(false);
 
     useEffect(() => {
         const accessToken = localStorage.getItem("access_token");
@@ -29,20 +29,22 @@ export function useRaket(id) {
     }, [id]);
 
     // raket application
-    const apply = async (raketistaId, priceProposal, budget) => {
-        setApplyLoading(true);
-        setApplyError("");
-        setApplySuccess(false);
-        const accessToken = localStorage.getItem("access_token");
-        try {
-            await applyToRaket({ raketId: id, raketistaId, priceProposal, budget }, accessToken);
-            setApplySuccess(true);
-        } catch (err) {
-            setApplyError(err.message);
-        } finally {
-            setApplyLoading(false);
-        }
-    };
+    // const apply = async (raketistaId, priceProposal, budget) => {
+    //     setApplyLoading(true);
+    //     setApplyError("");
+    //     setApplySuccess(false);
+    //     const accessToken = localStorage.getItem("access_token");
+    //     try {
+    //         await applyToRaket({ raketId: id, raketistaId, priceProposal, budget }, accessToken);
+    //         setApplySuccess(true);
+    //     } catch (err) {
+    //         setApplyError(err.message);
+    //     } finally {
+    //         setApplyLoading(false);
+    //     }
+    // };
 
-    return { raket, loading, error, apply, applyLoading, applyError, applySuccess };
+    return { raket, loading, error };
+        
+        // , apply, applyLoading, applyError, applySuccess };
 }
