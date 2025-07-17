8a35539e68d6e0e3045948408a1de391d6a9aad3
CODE-Der <xxxcagang@gmail.com>
2025-07-18 00:04:46 +0800

    fix: backend can now run in this branch

--- backend/src/entities/raket-application/raket-application.module.ts
+++ backend/src/entities/raket-application/raket-application.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RaketApplication } from './entities/raket-application.entity';
 import { RaketApplicationService } from './raket-application.service';
 import { RaketApplicationController } from './raket-application.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RaketApplication])],
+  imports: [TypeOrmModule.forFeature([RaketApplication]), UserModule],
   providers: [RaketApplicationService],
   controllers: [RaketApplicationController],
   exports: [RaketApplicationService],
