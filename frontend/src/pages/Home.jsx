11353431ca47657ada466193b6f78168196eea05
ldlambayao <187852863+ldlambayao@users.noreply.github.com>
2025-07-18 22:53:49 +0800

    feat: modified page with additional components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,31 @@
-// ito na yung home/landing page na magamit if "logged in" na si user
-
 import React from "react";
+import WelcomeBanner from "../components/WelcomeBanner";
+import SearchBar from "../components/SearchBar";
+import DashboardCardList from "../components/DashboardCardList";
 import SideNav from "../components/SideNav";
 
 const Home = () => {
+  const mockUser = {
+    fullName: "Daniya Reyes",
+  };
+
   return (
     <div className="flex">
       <SideNav />
-      <main className="flex-1 p-8">
-        <h1 className="text-2xl font-semibold">Homeeee</h1>
-        {/* Here ilagay ang content and shananigans */}
-      </main>
+
+      <div className="flex-1 relative min-h-screen bg-[#ffffff]">
+        {/* Top Banner */}
+        <WelcomeBanner fullName={mockUser.fullName} />
+
+        {/* Floating Search */}
+        <SearchBar />
+
+        {/* Main content (add padding to avoid overlap) */}
+        <div className="pt-28 px-8 space-y-6">
+          <DashboardCardList />
+        </div>
+        
+      </div>
     </div>
   );
 };
