af39b45f5106aa6502476e8b973337bd9d4ce689
{krystellar} <krystel.zerep@gmail.com>
2025-07-25 15:38:34 +0800

    chore: fix dependency cycle error

--- frontend/src/api/rakets.js
+++ frontend/src/api/rakets.js
@@ -94,6 +94,26 @@ export async function fetchApplicationsForRaket(raketId) {
   return response.json();
 }
 
+// fetching raket applications from the client side
+export async function fetchRaketApplications(accessToken) {
+  const response = await fetch('http://localhost:3000/raket-application', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  if (!response.ok) throw new Error('Failed to fetch raket applications');
+  return response.json();
+}
+// fetch application based on raket for filtering
+export async function fetchApplicationsForRaket(raketId, accessToken) {
+  const response = await fetch(`http://localhost:3000/raket-application/raket/${raketId}`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+  if (!response.ok) throw new Error('Failed to fetch applications based on the raket');
+  return response.json();
+}
+
+
 // applying to a raket
 export async function applyToRaket({ raketId, raketistaId, priceProposal }) {
   const response = await fetch('http://localhost:3000/raket-application', {
@@ -111,6 +131,7 @@ export async function applyToRaket({ raketId, raketistaId, priceProposal }) {
   if (!response.ok) throw new Error('Failed to apply to raket.');
   return response.json();
 }
+
 
 // accepting or rejecting raket application
 export async function acceptApplication(id) {
