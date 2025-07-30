704c32c12f1cfafec7605add9d9c2d027d337747
ldlambayao <187852863+ldlambayao@users.noreply.github.com>
2025-07-31 01:29:04 +0800

    fix: typo

--- frontend/src/components/AdminSideBar.jsx
+++ frontend/src/components/AdminSideBar.jsx
@@ -37,7 +37,7 @@ function AdminSidebar() {
             className="w-10 h-10 rounded-full"
           />
           <div>
-            <p className="text-sm font-medium">Admin Raket</p>
+            <p className="text-sm font-medium">Admin</p>
             <p className="text-xs text-gray-500">Administrator</p>
           </div>
         </div>
