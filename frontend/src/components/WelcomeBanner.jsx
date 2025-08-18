ba4fdd1ca7a35ad0c697ca294cfb95cad44e707e
mj <jhayegnzls@gmail.com>
2025-08-18 14:44:12 +0800

    fix: home page ui

--- frontend/src/components/WelcomeBanner.jsx
+++ frontend/src/components/WelcomeBanner.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const WelcomeBanner = ({ firstName }) => {
-  // const firstName = fullName.split(" ")[0];
-
   return (
-    <div className="relative w-full h-70 bg-gradient-to-r from-[#EFF6FF] to-[#FFF7ED] text-[#0C2C57] flex flex-col justify-center items-center px-6">
+    <div className="relative w-full h-70 bg-gradient-to-r from-[#EFF6FF] to-[#FFF7ED] text-[#0C2C57] flex flex-col justify-center items-center px-6 py-12 shadow-md">
       <h1 className="text-3xl font-bold mb-2">
-        Welcome back, {firstName}!
+        Welcome back, <span className="text-[#FF7C2B]">{firstName}</span>!
       </h1>
-      <p className="text-base">
-        Ready to find your next raket or post a new gig?
+      <p className="text-base text-[#374151]">
+        Ready to find your next{" "}
+        <span className="text-[#FF7C2B] font-medium">raket</span> or post a new
+        gig?
       </p>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,16 +12,14 @@ import LoadingSpinner from "../components/LoadingSpinner";
 
 const Home = () => {
   const { user, loading: authLoading } = useAuth();
-  const { rakets, loading: raketLoading} = useRakets();
+  const { rakets, loading: raketLoading } = useRakets();
   const [searchTerm, setSearchTerm] = useState("");
 
   if (authLoading) {
-  return (
-      <LoadingSpinner fullScreen />
-    );
+    return <LoadingSpinner fullScreen />;
   }
   if (!user) return <p>You are not logged in.</p>;
-  
+
   const filteredRakets = rakets.filter((raket) =>
     raket.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -39,19 +37,24 @@ const Home = () => {
 
         {/* Search */}
         <div className="absolute left-1/2 top-44 transform -translate-x-1/2 z-10 w-full px-4">
-          <SearchBar transparentBg searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <SearchBar
+            transparentBg
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+          />
         </div>
 
         {/* Main content */}
         <div className="pt-28 px-8 space-y-6 relative">
           <PostRaket />
-          <Help /> 
+          <Help />
           <DashboardCardList />
           {/* Show filtered rakets */}
           {searchTerm && (
             <div className="mt-5 mb-10">
-              <h2 className="text-xl font-semibold mb-4">
-                Search results for "{searchTerm}"
+              <h2 className="text-xl text-[#0c2c57] font-semibold mb-4">
+                Search results for{" "}
+                <span className="text-[#ff7c2b]">"{searchTerm}"</span>
               </h2>
 
               {raketLoading ? (
@@ -59,8 +62,8 @@ const Home = () => {
               ) : filteredRakets.length > 0 ? (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                   {filteredRakets.map((raket) => (
-                    <RaketCard 
-                      key={raket.raketId} 
+                    <RaketCard
+                      key={raket.raketId}
                       title={raket.title}
                       description={raket.description}
                       images={raket.images || []}
