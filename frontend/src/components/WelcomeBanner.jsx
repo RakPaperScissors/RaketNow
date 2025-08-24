a775b058467435e0a73ec06e468bd45b2bcfab05
ldlambayao <187852863+ldlambayao@users.noreply.github.com>
2025-08-24 16:32:47 +0800

    style: made the shadow more subtle

--- frontend/src/components/WelcomeBanner.jsx
+++ frontend/src/components/WelcomeBanner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WelcomeBanner = ({ firstName }) => {
   return (
-    <div className="relative w-full h-70 bg-gradient-to-r from-[#EFF6FF] to-[#FFF7ED] text-[#0C2C57] flex flex-col justify-center items-center px-6 py-12 shadow-md">
+    <div className="relative w-full h-70 bg-gradient-to-r from-[#EFF6FF] to-[#FFF7ED] text-[#0C2C57] flex flex-col justify-center items-center px-6 py-12 shadow-sm">
       <h1 className="text-3xl font-bold mb-2">
         Welcome back, <span className="text-[#FF7C2B]">{firstName}</span>!
       </h1>
